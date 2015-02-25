e1176c94832b0fd3bb1e3f933eb214af8888af8c
Omar Arguelles Arreola <omar00231@gmail.com>
2015-02-25 00:26:36 -0700

    Config: Add editorconfig and gitignore files

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,20 @@ var usersResource = new serverbone.resources.Resource('users', {
   collection: collections.Users
 });
 
+app.use(function(req, res, next) {
+  var method = req.method;
+  var modifyPassword = function() {
+    var hashedPassword = passwordHash.generate(req.body.password);
+    req.body.password = hashedPassword;
+  }
+
+  if (method === 'POST') { modifyPassword(); }
+
+  if (method === 'PUT' && req.body.password) { modifyPassword(); }
+
+  next();
+});
+
 var app = express();
 app.use(bodyParser());
 app.use('/users', usersResource.app);
