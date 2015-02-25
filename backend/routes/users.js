01dde44c2451b319fe53e44b070cee3af0541e89
Omar Arguelles Arreola <omar00231@gmail.com>
2015-02-25 03:38:16 -0700

    Backend: Basic crud routes support

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
-var client = require('../models/user');
+var User = require('../models/user');
 
 router.get('/', function(req, res) {
   res.render('index');
+})
+
+router.get('/users', function(req, res) {
+  User.find(function(err, users) {
+    if (err) { return res.send(err); }
+
+    res.json(users);
+  });
+})
+
+router.post('/users', function(req, res) {
+  var user = new User(req.body);
+
+  user.save(function(err) {
+    if (err) { return res.send(err); }
+
+    res.send({ message: 'User added' });
+  });
+})
+
+router.put('/users/:id', function(req, res) {
+  User.findOne({ _id: req.params.id }, function(err, user) {
+    if (err) { return res.send(err); }
+
+    for (prop in req.body) {
+      user[prop] = req.body[prop];
+    }
+
+    user.save(function(err) {
+      if (err) { return res.send(err); }
+
+      res.send({ message: 'User modified' });
+    });
+  });
+})
+
+router.get('/users/:id', function(req, res) {
+  User.findOne({ _id: req.params.id }, function(err, user) {
+    if (err) { return res.send(err); }
+
+    res.json(user)
+  });
 })
 
 module.exports = router;
